72bbcbf3ffc0fc9a0a319a0d6cc0f1bd6e35e41a
Aria Pahlavan <apahlavan1@utexas.edu>
2022-02-06 20:11:28 -0600

    Clean up home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface TimelineContent {
+  event: string;
+  icon: string;
+}
+
+interface ContactContent {
+  title: string;
+  url: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,5 +17,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(): void {}
+  aboutMeContent = {
+
+    avatarUrl: 'https://avatars.githubusercontent.com/u/8824819?s=400&u=caf1f3662ae94428df3b098a721cfa24d4b72bc0&v=4',
+    avatarAlt: 'Aria Pahlavan',
+    name: 'Aria Pahlavan',
+    summary: 'Sr. SW Engineer @ Visa',
+    description: `My talents are
+          aesthetically pleasing apps 🤩,
+          blazing fast apis 🔥,
+          scalable and reactive designs 🚀,
+          and trick shots in ping pong 🏓`
+  };
+
+  contactsContent: ContactContent[] = [
+    {title: 'GitHub', icon: 'fab fa-github-alt', url: 'https://github.com/ariapahlavan'},
+    {title: 'LinkedIn', icon: 'fab fa-linkedin-in', url: 'https://www.linkedin.com/in/ariapahlavan/'},
+    {title: 'Stack Overflow', icon: 'fab fa-stack-overflow', url: 'https://stackoverflow.com/users/6042837/aria-pahlavan?tab=profile'},
+    // {title: 'Twitter', icon: 'fab fa-twitter', url: '#'},
+    {title: 'Email', icon: 'fas fa-at', url: 'mailto:arix.ap@gmail.com'},
+    {title: 'Resume', icon: 'fas fa-align-left', url: '#'}
+  ];
+
+  timelineContent: TimelineContent[] = [
+    {event: 'Published an article about Progressive Web Apps', icon: '📄'},
+    {event: 'Co-authored and filed a patent at Visa', icon: '📜'},
+    {event: 'Senior Software Engineer at Visa', icon: '🙌'},
+    {event: 'Published an article about Reactive Programming', icon: '📄'},
+    {event: 'Published an article about Functional Programming', icon: '📄'},
+    {event: 'Software Engineer at Visa', icon: '🧑‍💻'},
+    {event: 'Graduated from The University of Texas at Austin', icon: '👨‍🎓'},
+    {event: 'Software Engineer Intern at Intel', icon: '🧑‍💻'},
+    {event: 'Electrical Engineer Intern at Luminant', icon: '🧑‍💻'},
+    {event: 'Software Engineer Intern at Texas Petawatt Laser', icon: '🧑‍💻'},
+    {event: 'Transferred to The University of Texas at Austin', icon: '🏫'},
+    {event: 'Cashier at Walmart', icon: '🛒'},
+    {event: 'Started college at Collin College', icon: '🏫'},
+    {event: 'Moved to US', icon: '✈️'},
+    {event: 'Studied at Firooz Bahram High School', icon: '🏫'},
+    {event: 'Studied at Rostam Abadian Middle School', icon: '🏫'},
+    {event: 'Studied at Jamshid Jam Elementary School', icon: '🏫'},
+    {event: 'Was born in Tehran, IR', icon: '🍼'},
+  ];
+
+  ngOnInit(): void {
+  }
 }
