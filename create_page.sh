7a715332d679780a1ee303a8b1e0c27dbd5a6226
Aria Pahlavan <apahlavan1@utexas.edu>
2022-02-13 18:29:14 -0600

    Update create page script

--- create_page.sh
+++ create_page.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
   then
     printf "╭───────────────────────────────────────────────────────────────╮\n"
-    printf "│ Usage: sh <script_name>.sh <post_name>                        │\n"
+    printf "│ Usage: sh <script_name>.sh <page_name> <page_title>           │\n"
     printf "╰───────────────────────────────────────────────────────────────╯\n"
     exit 1
 fi
 page_name=$1
+page_title=$2
 page_dir=details/pages/${page_name}
 
 printf "RUN: 'ng g c %s -m details'\n" "${page_dir}"
 ng g c "${page_dir}" -m details
+
+sh post_gen.sh articles "${page_title}" "Summary for ${page_title}"
