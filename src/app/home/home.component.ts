a4864a53bab496aeccebb42fba8c9ef6337753c3
Aria Pahlavan <apahlavan1@utexas.edu>
2022-02-28 12:28:34 -0600

    Remove static resume

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
     {title: 'Stack Overflow', icon: 'stack-overflow', url: 'https://stackoverflow.com/users/6042837/aria-pahlavan?tab=profile', isSvg: true},
     // {title: 'Twitter', icon: 'twitter', url: '#', isSvg: true},
     {title: 'Email', icon: 'alternate_email', url: 'mailto:arix.ap@gmail.com'},
-    {title: 'Resume', icon: 'description', url: `${env.assetsPath}/Aria-Pahlavan-Resume.pdf`}
+    {title: 'Resume', icon: 'description', url: 'https://drive.google.com/file/d/1fg05gafB_x4u6Z3DakKk-KrFYORSWLLD/view'}
   ];
 
   timelineContent: TimelineContent[] = [
